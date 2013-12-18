dad7e428d691fe0e1ab1d058f30321cff9ebfc1c
go1dfish <justageekbiz@gmail.com>
2013-12-17 21:55:01 -0800

    Add google analytics tracking

--- js/router.js
+++ js/router.js
@@ -1,3 +1,13 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-2878955-3']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+
 var Dig = Em.Application.create();
 Dig.deferReadiness();
 
@@ -88,6 +98,13 @@ Dig.ApplicationRoute = Em.Route.extend({
     },
     dig: function(queryParams) {
       this.transitionTo('uploads', queryParams);
+    },
+    didTransition: function(){
+      var router = this.router;
+      Ember.run.once(function(){
+        // Track this pageview with Google analytics
+        _gaq.push(['_trackPageview', router.get('url')]);
+      });
     }
   }
 });
