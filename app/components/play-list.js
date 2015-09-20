99640f0df4e9731371a3736699c45fea42b89cad
Victor Stone <victor.stone@gmail.com>
2015-09-20 02:48:55 -0700

    download popup from playlist was wrong

--- app/components/play-list.js
+++ app/components/play-list.js
@@ -9,7 +9,16 @@ export default Ember.Component.extend({
     },  
   
     popup: function(name,hash) {
-      this.sendAction('popup',name,hash);
+      // hack this in for now
+      if( name === 'download' ) {
+        var store = this.container.lookup('store:uploads');
+        store.info(hash.upload.id).then( u => {
+          hash.upload = u;
+          this.sendAction('popup',name,hash);
+        });
+      } else {      
+        this.sendAction('popup',name,hash);
+      }
     }
   }
 
